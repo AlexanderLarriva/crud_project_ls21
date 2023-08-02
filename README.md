d81df1244374e0bd6442381eb135736117c7f5a8
Alexander Shangin <alarriva@yandex.ru>
2023-08-02 15:28:57 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# crud_project_ls21
+
+## Практическая задача к занятию № 21 Hexlet - CRUD: Cookies
+
+Practical task for lesson # 21 - CRUD: Cookies
---
+++ crud_project_ls21/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, json, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    cart = json.loads(request.cookies.get('cart', json.dumps({})))
+    return render_template('index.html', cart=cart)
+
+# BEGIN (write your solution here)
+
+# END
